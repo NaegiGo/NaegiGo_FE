90925998a05ca2d5e351385e9a9fb288eea5ad5c
seunghyeonKang <number42isuser@gmail.com>
2026-09-20 10:51:59 +0900

    🚚 Mod: 이름 수정 페이지 (main) 그룹으로 이동

--- src/app/(main)/name/edit/page.tsx
+++ src/app/(main)/name/edit/page.tsx
@@ -1,9 +1,5 @@
 import { PagePlaceholder } from "@/components/common/PagePlaceholder";
 
 export default function ProfileEditPage() {
-  return (
-    <PagePlaceholder 
-      title="이름 수정 페이지" 
-    />
-  );
+  return <PagePlaceholder title="이름 수정 페이지" />;
 }
